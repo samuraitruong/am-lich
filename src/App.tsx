5d96b8dd682ea969c38ca948fbaaa107ebc71639
Truongg Nguyen <samuraitruong@hotmail.com>
2018-11-14 20:05:41 +1100

    part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { CAN, CHI, MONTHS } from "./Constants";
 import { convertSolar2Lunar } from "lunardate";
 import "./scss/App.css";
 interface IAppState {
@@ -13,8 +14,9 @@ IAppState > {
     }
   }
   public render() {
-    console.log(convertSolar2Lunar(1, 1, 2018, 0));
     const selectedDate = this.state.selectedDate;
+    const ln = convertSolar2Lunar(selectedDate.getDate(), selectedDate.getMonth() + 1, selectedDate.getFullYear(), 7);
+    console.log(ln.toDate);
     const dayOfWeeks = [
       "Chủ nhật",
       "Thứ hai",
@@ -24,6 +26,10 @@ IAppState > {
       "Thứ sáu",
       "Thứ bảy"
     ]
+    // const jd = ln.toJd; console.log(jd);
+    const jd = this.jdFromDate(selectedDate.getDate(), selectedDate.getMonth() + 1, selectedDate.getFullYear());
+    const thang = CAN[(ln.year * 12 + ln.month + 3) % 10] + " " + CHI[(ln.month + 1) % 12];
+    const ngay = CAN[(jd + 9) % 10] + " " + CHI[(jd + 1) % 12];
     return (
       <div className="app">
         <div className="calendar">
@@ -35,12 +41,61 @@ IAppState > {
             {selectedDate.getDate()}
             <span className="calendar--day--weekday">{dayOfWeeks[selectedDate.getDay()]}</span>
           </div>
+          <div className="calendar--lunar">
+            < div className="calendar--lunar--date">
+              <div>Tháng {MONTHS[ln.month - 1]}</div>
+              <div>{ln.day}</div>
+              <div>Năm {this.getYearCanChi(ln.year)}</div>
+            </div >
+            < div className="calendar--lunar--info">
+              <div>Tháng {thang}</div>
+              <div>Tháng {ngay}</div>
+              <div>
+                Giờ {this.getCanHour0(jd) + " " + CHI[selectedDate.getHours() % 12]}
+              </div>
+            </ div>
+            <div className="calendar--lunar--perfect-hours">
+              Giờ hoàng đạo : {this.getGioHoangDao(jd)}
+            </div>
+          </div>
         </div>
-        <header className="app-header">
-          <span/>
-        </header>
       </div>
     );
+  }
+  private getGioHoangDao(jd : number) {
+    const chiOfDay = (jd + 1) % 12;
+    const GIO_HD = new Array("110100101100", "001101001011", "110011010010", "101100110100", "001011001101", "010010110011");
+    const gioHD = GIO_HD[chiOfDay % 6]; // same values for Ty' (1) and Ngo. (6), for Suu and Mui etc.
+    let ret = "";
+    let count = 0;
+    for (let i = 0; i < 12; i++) {
+      if (gioHD.charAt(i) === '1') {
+        ret += CHI[i];
+        ret += ' (' + (i * 2 + 23) % 24 + '-' + (i * 2 + 1) % 24 + ')';
+        if (count++ < 5) {
+          ret += ', ';
+        }
+      }
+    }
+    return ret;
+  }
+
+  private jdFromDate(dd : number, mm : number, yy : number) {
+    const INT = (n : number) => Math.floor(n);
+    const a = INT((14 - mm) / 12);
+    const y = yy + 4800 - a;
+    const m = mm + 12 * a - 3;
+    let jd = dd + INT((153 * m + 2) / 5) + 365 * y + INT(y / 4) - INT(y / 100) + INT(y / 400) - 32045;
+    if (jd < 2299161) {
+      jd = dd + INT((153 * m + 2) / 5) + 365 * y + INT(y / 4) - 32083;
+    }
+    return jd;
+  }
+  private getCanHour0(jdn : number) {
+    return CAN[(jdn - 1) * 2 % 10];
+  }
+  private getYearCanChi(year : number) {
+    return CAN[(year + 6) % 10] + " " + CHI[(year + 8) % 12];
   }
 }
 
---
+++ src/Constants.ts
@@ -0,0 +1,40 @@
+export const MONTHS = [
+    "Giêng",
+    "Hai",
+    "Ba",
+    "Tư",
+    "Năm",
+    "Sáu",
+    "Bảy",
+    "Tám",
+    "Chín",
+    "Mười",
+    "Muời môt",
+    "Chạp"
+]
+export const CAN = [
+    "Giáp",
+    "Ất",
+    "Bính",
+    "Đinh",
+    "Mậu",
+    "Kỳ",
+    "Canh",
+    "Tân",
+    "Nhâm",
+    "Quý"
+];
+export const CHI = [
+    "Tý",
+    "Sửu",
+    "Dần",
+    "Mẹo",
+    "Thìn",
+    "Tỵ",
+    "Ngọ",
+    "Mùi",
+    "Thân",
+    "Dậu",
+    "Tuất",
+    "Hợi"
+];
